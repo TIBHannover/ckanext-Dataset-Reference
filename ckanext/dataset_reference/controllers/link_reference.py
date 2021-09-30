b6672b8a9e3f88a9d6f2a3dc5a5595efecee52a9
pooya.oa <pooya.oladazimi@tib.eu>
2021-09-30 12:27:24 +0200

    removed months field from view

--- ckanext/dataset_reference/controllers/link_reference.py
+++ ckanext/dataset_reference/controllers/link_reference.py
@@ -95,13 +95,11 @@ class LinkReferenceController():
         package = toolkit.get_action('package_show')({}, {'name_or_id': package_name})
         publication_types = Helper.get_publication_types_dropdown_content()
         years = Helper.get_years_list()
-        months = Helper.get_month_list()
 
         return render_template('add_manually.html', 
             pkg_dict=package, 
             publication_types=publication_types,
             years=years,
-            months=months
             )
     
 
