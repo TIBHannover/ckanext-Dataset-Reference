05de8cbe25e2f1697c0bf5a51e3c2acdb07b587f
pooya.oa <pooya.oladazimi@tib.eu>
2021-09-30 10:36:29 +0200

    updated the model

--- ckanext/dataset_reference/models/package_reference_link.py
+++ ckanext/dataset_reference/models/package_reference_link.py
@@ -17,14 +17,46 @@ package_reference_link_table = Table(
     Column(u"doi", _types.UnicodeText, nullable=False),    
     Column(u"create_at", _types.DateTime, default=datetime.datetime.utcnow, nullable=False),
     Column(u"citation", _types.UnicodeText),
+    Column(u"authors", _types.UnicodeText),
+    Column(u"title", _types.UnicodeText),
+    Column(u"year", _types.UnicodeText),
+    Column(u"url", _types.UnicodeText),
+    Column(u"ref_type", _types.UnicodeText),
+    Column(u"publisher", _types.UnicodeText),
+    Column(u"place", _types.UnicodeText),
+    Column(u"journal", _types.UnicodeText),
+    Column(u"volume", _types.UnicodeText),
+    Column(u"issue", _types.UnicodeText),
+    Column(u"page", _types.UnicodeText),
+    Column(u"proceeding", _types.UnicodeText),
+    Column(u"access_date", _types.UnicodeText),
+    Column(u"organization", _types.UnicodeText),
+    Column(u"thesis_type", _types.UnicodeText),
+    
 )
 
+
 class PackageReferenceLink(domain_object.DomainObject):
-    def __init__(self, package_name=None, doi=None, create_at=None, citation=None):
-        self.package_name = package_name
-        self.doi = doi        
-        self.create_at = create_at
-        self.citation = citation
+    def __init__(self, reference_object):
+        self.package_name = reference_object['package_name']
+        self.doi = reference_object['doi']        
+        self.create_at = reference_object['create_at']
+        self.citation = reference_object['citation']
+        self.authors = reference_object['authors']
+        self.title = reference_object['title']
+        self.year = reference_object['year']
+        self.url = reference_object['url']
+        self.ref_type = reference_object['ref_type']
+        self.publisher = reference_object['publisher']
+        self.place = reference_object['place']
+        self.journal = reference_object['journal']
+        self.volume = reference_object['volume']
+        self.issue = reference_object['issue']
+        self.page = reference_object['page']
+        self.proceeding = reference_object['proceeding']
+        self.access_date = reference_object['access_date']
+        self.organization = reference_object['organization']
+        self.thesis_type = reference_object['thesis_type']
 
     
     @classmethod
