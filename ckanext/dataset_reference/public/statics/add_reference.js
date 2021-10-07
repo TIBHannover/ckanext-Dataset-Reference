b3c56614120cd584c88f9f889396a71e73c45ae3
pooya.oa <pooya.oladazimi@tib.eu>
2021-10-07 12:10:07 +0200

    change the text input background when not focused

--- ckanext/dataset_reference/public/statics/add_reference.js
+++ ckanext/dataset_reference/public/statics/add_reference.js
@@ -16,5 +16,22 @@ $(document).ready(function(){
             }
         }); 
     });
+
+    $('#doi').focusin(function(){
+        $('#doi').css("background-color", 'white');
+        $('#doi').addClass('link-enabled');
+        $('#bibtex').removeClass('link-enabled');
+        $('#bibtex').css("background-color", '#EBEBEB');
+    });
+
+
+    $('#bibtex').focusin(function(){
+        $('#bibtex').css("background-color", 'white');
+        $('#bibtex').addClass('link-enabled');
+        $('#doi').css("background-color", '#EBEBEB');
+        $('#doi').removeClass('link-enabled');
+    });
+
+    
    
 });
